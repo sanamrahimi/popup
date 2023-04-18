7d5afa1e75e220ec00e526d2e19af98e25682c5f
Sanam Rahimi <sanam.rahimi099@gmail.com>
2023-04-18 22:16:38 +0500

    add changes

--- script.js
+++ script.js
@@ -167,8 +167,7 @@ seeBtn.forEach((seeBtn) => {
         <li>GitHub</li>
         <li>JavaScript</li> 
         <li>Bootstrap</li>
-        <li>Terminal</li>
-        <li>Codepen</li> 
+        
 
       </ul>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
